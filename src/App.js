c8acd7762d9c01cff9c0fe272354e2fedbf6925c
Mauricio Costa <maurito11@gmail.com>
2018-09-08 10:40:34 -0300

    Cleaned Pre Initial Folders Structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App App-header">
+        Mauri's Graphing Calculator Kick-Off
+      </div>
+    );
+  }
+}
+
+export default App;
