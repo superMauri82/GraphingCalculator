25cbe9d670486e34db698592e3452b1a8ab2d85f
Mauricio Costa <maurito11@gmail.com>
2018-09-14 13:57:58 -0300

    Rearranged store instantiation

--- src/components/CalculatorContainer.jsx
+++ src/components/CalculatorContainer.jsx
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
 import { Grid, Segment } from 'semantic-ui-react'
 import FeedbackDisplayContainer from '../containers/FeedbackDisplayContainer'
-import KeyboardAndDisplay from './Keyboard'
+import KeyboardContainer from '../containers/KeyboardContainer'
 import GraphicContainer  from '../containers/GraphicContainer'
 import ProjectHeader from './ProjectHeader'
 import lib from '../lib/lib'
+import { Provider } from 'react-redux'
+
+import storeFactory from '../store/storeFactory'
+
+const store = storeFactory()
 
 const CalculatorContainer = () => (
-
+  <Provider store={store}>
     <Grid container stacked divided columns={2}>
-
       <Grid.Row stretched >
-          <ProjectHeader/>
+          <ProjectHeader />
       </Grid.Row>
       <Grid.Row stretched >
         <Grid.Column>
@@ -19,7 +23,7 @@ const CalculatorContainer = () => (
             <FeedbackDisplayContainer />
           </Segment>
           <Segment>
-            <KeyboardAndDisplay/>
+            <KeyboardContainer />
           </Segment>
         </Grid.Column>
         <Grid.Column>
@@ -29,6 +33,7 @@ const CalculatorContainer = () => (
         </Grid.Column>
       </Grid.Row>
     </Grid>
+  </Provider>
 
 )
 
