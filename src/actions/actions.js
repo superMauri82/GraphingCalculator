fd4aba2d7108532a5067e57dfbc2d33ee1e24601
Mauricio Costa <maurito11@gmail.com>
2018-09-14 13:54:57 -0300

    Refactor of Action Creators

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,11 +1,31 @@
-import C from './Constants'
-import { v4 } from 'uuid'
+import C from './constants'
+import { isSafeExpression, getCreationTime, getId } from '../lib/lib'
 
-export const updateFeedbackStatus = 
-    ( statusCode=0, msg='')=>
+const errorExpression = ( expression='' ) =>
       ({
-          statusCode,
-          msg,
-          type: C.UPDATE_FEEDBACK_STATUS
+          expression: expression,
+          type: C.ERROR_EXPRESSION
       })
 
+const saveExpression = ( expression='' ) =>
+      ({
+          id: getId(),
+          createdAt: getCreationTime(),
+          expression: expression,
+          type: C.SAVE_EXPRESSION
+      })
+
+export const saveOrErrorExpression = ( expression='' ) =>
+    isSafeExpression(expression) ? saveExpression(expression) : errorExpression(expression)
+
+export const removeExpression = ( id ) =>
+      ({
+          ...id,
+          type: C.REMOVE_EXPRESSION
+      })
+
+export const updateCurrentExpression = ( partialExpression ) =>
+      ({
+          expression: partialExpression,
+          type: C.UPDATE_CURRENT_EXPRESSION
+      })
