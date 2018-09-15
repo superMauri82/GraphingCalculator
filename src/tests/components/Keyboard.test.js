13a80f9dbc863a2898057ec983186ba903113bf8
Mauricio Costa <maurito11@gmail.com>
2018-09-15 18:45:02 -0300

    Keyboard Test Added

--- src/tests/components/Keyboard.test.js
+++ src/tests/components/Keyboard.test.js
@@ -1,17 +1,29 @@
 import React from 'react'
-import { configure, shallow } from 'enzyme'
+import { configure, shallow, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import should from 'should'
 import Keyboard from '../../components/Keyboard'
 
 configure({ adapter: new Adapter() })
 
+  
 
 describe('Keyboard Test Suite', () => {
-    describe('Keyboard', () =>{
-        it('should render/display always',() => { 
-            const keyboard = shallow(<Keyboard input='cos(x)' />) 
 
-            expect(keyboard.html()).toBeTruthy()
-        })
+    let _press,
+        _update,
+        wrapper,
+        props
+
+
+    beforeAll(() => {
+         _press  = jest.fn()
+         _update = jest.fn()
+         wrapper = mount(<Keyboard input={''} onExpressionUpdate={_update} onChangeWrapper={_press} />)
+         props = wrapper.find('div.simple-keyboard').props()
+    })
+
+    it('prop className should be simple-keyboard', () => {
+        props.className.should.eql('simple-keyboard')
     })
 })
